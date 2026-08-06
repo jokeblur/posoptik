3da7d852e4c7e3e83e760616e530b3ec9e217b07
jokeblur <dickywahyu3129@gmail.com>
2026-08-06 09:39:04 +0700

    perbaikan dan penambahan kwitansi

--- resources/views/kwitansi/create.blade.php
+++ resources/views/kwitansi/create.blade.php
@@ -52,7 +52,7 @@
                     <div class="row">
                         <div class="form-group col-md-6">
                             <label for="nama_pembuat">Nama Pembuat Kwitansi</label>
-                            <input type="text" class="form-control" id="nama_pembuat" name="nama_pembuat" value="{{ old('nama_pembuat') }}" placeholder="Contoh: Rina Putri">
+                            <input type="text" class="form-control" id="nama_pembuat" name="nama_pembuat" value="{{ old('nama_pembuat') }}" placeholder="Contoh: Udin Kasep">
                         </div>
                     </div>
                 </div>
